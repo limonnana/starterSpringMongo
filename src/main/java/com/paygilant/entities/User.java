b842316c87232ecc11071987b3da80ab4d0012af
eyaloffla <eyalr@off-la.com>
2017-12-03 18:13:19 +0200

    first commit

---
+++ src/main/java/com/paygilant/entities/User.java
@@ -0,0 +1,53 @@
+package com.paygilant.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+	
+	@Id
+    private long id;
+	private String name;
+	private String lastName;
+	
+	@Indexed(unique = true)
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/paygilant/starter03/Starter03Application.java
@@ -0,0 +1,12 @@
+package com.paygilant.starter03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starter03Application.class, args);
+	}
+}
