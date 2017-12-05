14430c056d3bb109c519d713375b8403bd5332a1
eyaloffla <eyalr@off-la.com>
2017-12-05 18:01:40 +0200

    dec 5

---
+++ src/main/java/com/paygilant/config/Config.java
@@ -0,0 +1,7 @@
+package com.paygilant.config;
+
+public class Config {
+	
+	public static final String APP_NAME = "/starter";
+
+}
--- src/main/java/com/paygilant/ws/UserWS.java
+++ src/main/java/com/paygilant/ws/UserWS.java
@@ -1,19 +1,25 @@
 package com.paygilant.ws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.paygilant.clients.UserClient;
+import com.paygilant.config.Config;
+
 
 @RestController
 public class UserWS {
 	
-	@Autowired
-	UserClient userClient;
+	private static final Logger LOGGER =  LoggerFactory.getLogger(UserWS.class);
 	
-	@RequestMapping("/users")
+	@Autowired
+	private UserClient userClient;
+	
+	@RequestMapping(Config.APP_NAME + "/users")
 	public String getUsers() {
+		
 		return userClient.getUsers();
 	}
 
